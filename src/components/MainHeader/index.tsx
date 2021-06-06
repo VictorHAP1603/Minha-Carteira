53fa4901d8d928659a04750b2bdd18f6549b1446
VictorHugo1603 <vitor.hugo@newpost.com>
2021-06-05 21:44:40 -0300

    configurações iniciais

---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./style";
+
+const MainHeader: React.FC = () => {
+  return (
+    <Container>
+      <h1>Header</h1>
+    </Container>
+  );
+};
+
+export default MainHeader;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SigIn: React.FC = () => {
+  return <div>SignIn</div>;
+};
+
+export default SigIn;
