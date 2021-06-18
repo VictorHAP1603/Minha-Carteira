cc47b0b7cdf04bb62dff89979e80aca28cc7a4b0
VictorHugo1603 <vitor.hugo@newpost.com>
2021-06-17 23:34:22 -0300

    começando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 import ContentHeader from "../../components/ContentHeader/index";
 import SelectInput from "../../components/SelectInput";
+import { Months } from "../../utils/months";
+
+import { expenses } from "../../utils/expenses";
+import { gains } from "../../utils/gains";
 
 import { Container } from "./style";
 
 const Dashboard: React.FC = () => {
-  const options = [
-    { value: "Rodrigo", label: "Rodrigo" },
-    { value: "Maria", label: "Maria" },
-    { value: "Joao", label: "Joao" },
-  ];
+  const [monthSelected, setMonthSelected] = useState<string>(
+    String(new Date().getMonth() + 1)
+  );
+  const [yearSelected, setYearSelected] = useState<string>(
+    String(new Date().getFullYear())
+  );
 
   const titleProps = {
     title: "Dashboard",
     lineColor: "#e44c4e",
   };
 
+  const months = useMemo(() => {
+    return Months.map((month, index) => ({ value: index + 1, label: month }));
+  }, []);
+
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    [...gains, ...expenses].forEach(({ date }) => {
+      const year = new Date(date).getFullYear();
+      if (!uniqueYears.includes(year)) uniqueYears.push(year);
+    });
+
+    return uniqueYears
+      .sort((a, b) => b - a)
+      .map((year) => ({ value: year, label: year }));
+  }, []);
+
   return (
     <Container>
       <ContentHeader title={titleProps.title} color={titleProps.lineColor}>
-        <SelectInput options={options} onChange={() => {}} />
+        <SelectInput
+          options={months}
+          onChange={(e) => setMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => setYearSelected(e.target.value)}
+          defaultValue={yearSelected}
+        />
       </ContentHeader>
     </Container>
   );
